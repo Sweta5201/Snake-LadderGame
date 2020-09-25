5ae676b711b698d0beb802f4bd1176e22c4ef2e2
Sweta5201 <sweta65959@gmail.com>
2020-09-25 13:27:49 +0530

    second usecase done

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,8 +1,13 @@
 class SnakeLadder
 {
  public static final int POS = 0;
+ public static final int MAX = 7;
+ public static final int MIN = 1;
  public static void main(String[] args)
   {
     System.out.println("Single Player Starting at position "+ POS);
+    int number =(int) Math.floor(Math.random() * (MAX - MIN))+1;
+    System.out.println("The number on die is : "+number);
+
   }
 }
